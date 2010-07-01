b742408dc00c53e948b5b4a96cca8ba59cf86660
nagi <natsag2000@gmail.com>
2010-07-01 12:07:08 +0200

    added more feature to dot-mew

--- dot-mew.el
+++ dot-mew.el
@@ -1,12 +1,15 @@
-;;  ___    __  __   _   ___ _
-;; | __|__|  \/  | /_\ |_ _| |
-;; | _|___| |\/| |/ _ \ | || |__
-;; |___|  |_|  |_/_/ \_\___|____|
-;;
-;; Author: Nagi
-;; Description: my email account settings
-;;
+;; ------------------------------------------------;;
+;;  __  ____   __  __  __   _   ___ _
+;; |  \/  \ \ / / |  \/  | /_\ |_ _| |
+;; | |\/| |\ V /  | |\/| |/ _ \ | || |__
+;; |_|  |_| |_|   |_|  |_/_/ \_\___|____|
 
+;; shameless copied from:
+;; http://cermics.enpc.fr/~lelong/Emacs/dotmewdotel.html
+;; ------------------------------------------------;;
+
+
+;; ------------------------------------------------;;
 ;; mail accounts
 (setq mew-config-alist
       ;; GMAIL IMAP
@@ -15,34 +18,88 @@
          ("mailbox-type". imap)
          ("proto"          . "%")
          ("name"           . "Nagi")
-         ("user"           . "USERNAME@googlemail.com")
+         ("user"           . "USERNAME@googlemail.com")  ;; to be completed
          ("imap-server"    . "localhost")
          ("imap-delete" . nil)
          ("imap-size" . 0)
          ("imap-ssl"       . t)
-         ("imap-user"      . "USERNAME@googlemail.com")
+         ("imap-user"      . "USERNAME@googlemail.com")  ;; to be completed
          ("imap-ssl-port"  . "9930")
+         ("fcc" . "%Sent")
+         ("cc" . "")
+         ("imap-trash-folder" . "%Trash")
          ;; SMTP
          ("smtp-ssl"       . t)
          ("smtp-ssl-port"  .  "4650")
          ("smtp-auth-list" . ("PLAIN" "LOGIN" "CRAM-MD5"))
-         ("smtp-user"      . "USERNAME@googlemail.com")
+         ("smtp-user"      . "USERNAME@googlemail.com")   ;; to be completed
          ("smtp-server"    . "localhost")
          ("mail-domain"    . "localhost")
-         ))
+         )
+        ("local"
+         ("mailbox-type" . mbox)
+         ("proto" . "+")
+         ("name" . " ")                                      ;; to be completed
+         ("mbox-command-arg" . "-c -u -d /var/mail/USERNAME") ;; to be completed
+         ("inbox-folder" . "+system")
+         )
+        )
       )
+;; ------------------------------------------------;;
 
-;; general configuration for all account
-(setq mew-smtp-ssl-port "4650")
-;;(setq mew-prog-ssl "/usr/bin/stunnel")
-;;(setq mew-smtp-port "25")
-(setq mew-pop-size 0)
-;;(setq toolbar-mail-reader 'Mew)
-(setq mew-use-cached-passwd t)
-(setq mew-passwd-timer-unit 999)
-(setq mew-passwd-lifetime 999)
-(set-default 'mew-decode-quoted 't)
-(setq mew-pop-delete nil)
+
+
+;; ------------------------------------------------;;
+;; general config for all accounts
+
+(setq mew-smtp-ssl-port "4650")  ;; change it for your
+;;(setq mew-prog-ssl "/opt/local/bin/stunnel")
 (setq mew-ssl-verify-level 0)
+(setq mew-pop-auth 'pass) 
+(setq mew-pop-size 0)
+;;(setq mew-smtp-port "25")
+(setq mew-mbox-command "incm")
+(setq mew-auto-flush-queue t)
+(setq mew-decode-broken t)
+(setq mew-summary-form-mark-spam t)
+(set-default 'mew-decode-quoted 't)
 
+(setq mew-pop-delete t)
+(setq mew-ssl-verify-level 0)
+;; ------------------------------------------------;;
+
+
+
+;; ------------------------------------------------;;
+;; Gnupg
+;; (setq mew-protect-privacy-always t)
+;; (setq mew-protect-privacy-always-type 'pgp-signature)
 (setq mew-use-pgp-cached-passphrase t)
+;; (setq mew-pgp-server-url-template "http://pgp.mit.edu:11371/pks/lookup?op=get&search=0x4BB3C992")
+;; ------------------------------------------------;;
+
+
+
+;; ------------------------------------------------;;
+;; default paths
+(setq mew-mail-path "~/.Mail-Mew")
+(setq mew-conf-path mew-mail-path)
+(setq mew-addrbook-file "~/dotfiles/.Addressbook")
+;; ------------------------------------------------;;
+
+
+;; ----------------------------------------------- ;;
+;; Password
+(setq mew-use-cached-passwd t)     ;; nil
+(setq mew-passwd-timer-unit 1)     ;; 10 (minutes)
+(setq mew-passwd-lifetime 300000)  ;; 2
+;; ----------------------------------------------- ;;
+
+
+;; ----------------------------------------------- ;;
+;; Printing mail
+;;(setq mew-field-for-printing '("Subject:" "From:" "To:" "Cc:" "Date:"))
+;;(setq mew-print-function 'ps-print-buffer)
+;; ----------------------------------------------- ;;
+
+
